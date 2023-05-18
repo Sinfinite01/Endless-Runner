20a1d35a81c5f2a8b7ed0fe621a07e82630609ea
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-05-18 14:45:57 -0700

    push my worthless code that doesn't work

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ let config = {
         pixelArt: true
     },
     backgroundColor: '#ADD8E6',
-    width: 640,
-    height: 240,
+    width: 840,
+    height: 525,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,3 +16,5 @@ let config = {
 }
 
 const game = new Phaser.Game(config)
+
+let tileSize = 50;
--- src/scenes/Road.js
+++ src/scenes/Road.js
@@ -5,10 +5,45 @@ class Road extends Phaser.Scene{
     }
 
     preload(){
-
+        this.load.image('car','./assets/orangeTruck.png');
+        this.load.image('road','./assets/road.png');
     }
 
     create(){
+
+        // make ground tiles group
+        
+        /*this.ground = this.add.group();
+        for(let i = 0; i < game.config.width; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'road').setScale(SCALE).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        for(let i = tileSize*14; i < game.config.width-tileSize*5; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize*5, 'road').setScale(SCALE).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        for(let i = tileSize*2; i < game.config.width-tileSize*18; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize*9, 'road').setScale(SCALE).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }*/
+
+        //create Car
+        this.car01 = this.physics.add.sprite(game.config.width/5, game.config.height/2, 'car').setScale(1);
+        this.car01.body.allowGravity = true;
+        this.car01.body.immovable = false;
+
+
+
+        this.car01.setCollideWorldBounds(true);
+
+        // add physics collider
+        //this.physics.add.collider(this.car, this.ground);
 
     }
 
