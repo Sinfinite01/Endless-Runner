2a518dbd99d513c7892095dc79c71b2a313ad72d
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-06-04 16:24:16 -0700

    made space between arrows bigger, its now easier

--- src/scenes/Road.js
+++ src/scenes/Road.js
@@ -193,9 +193,9 @@ class Road extends Phaser.Scene{
         //this.arrow3 = new Arrow(this, game.config.width + this.arrow1.width*5, game.config.height*Math.random(), 'arrow', 0).setOrigin(0.5, 0.5);
 
         this.arrow1 = this.physics.add.sprite(game.config.width + this.arrow1.width, game.config.height*Math.random(), 'arrow').setOrigin(0.5, 0.5)
-        this.arrow2 = this.physics.add.sprite(game.config.width + this.arrow1.width*3.5, game.config.height*Math.random(), 'arrow').setOrigin(0.5, 0.5)
-        this.arrow3 = this.physics.add.sprite(game.config.width + this.arrow1.width*5.5, game.config.height*Math.random(), 'arrow').setOrigin(0.5, 0.5)
-        this.darkArrow1 = this.physics.add.sprite(game.config.width + this.arrow1.width*7.5, game.config.height*Math.random(), 'darkArrow').setOrigin(0.5, 0.5)
+        this.arrow2 = this.physics.add.sprite(game.config.width + this.arrow1.width*4, game.config.height*Math.random(), 'arrow').setOrigin(0.5, 0.5)
+        this.arrow3 = this.physics.add.sprite(game.config.width + this.arrow1.width*6, game.config.height*Math.random(), 'arrow').setOrigin(0.5, 0.5)
+        this.darkArrow1 = this.physics.add.sprite(game.config.width + this.arrow1.width*8, game.config.height*Math.random(), 'darkArrow').setOrigin(0.5, 0.5)
 
         this.arrow1.body.allowGravity = false;
         this.arrow1.body.immovable = false;
