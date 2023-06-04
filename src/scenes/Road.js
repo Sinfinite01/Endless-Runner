1a36129f67c7c1844635bc9216f636d26be7de38
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-06-04 15:39:57 -0700

    changing gravity name see if it helps

--- src/scenes/Road.js
+++ src/scenes/Road.js
@@ -280,7 +280,7 @@ class Road extends Phaser.Scene{
 
         this.explosionSound = this.sound.add('sfx_explosion').setVolume(0.5)
 
-        this.gravitySound = this.sound.add('sfx_gravity').setVolume(0.6)
+        this.gravitySound = this.sound.add('sfx_grav').setVolume(0.6)
 
         this.arrowSound = this.sound.add('sfx_arrow').setVolume(0.4)
 
