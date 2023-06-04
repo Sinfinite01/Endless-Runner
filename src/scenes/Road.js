269baa48ab1d4377b5fcd808031bc4985bf8c1c1
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-06-04 15:58:24 -0700

    changed audio effect

--- src/scenes/Road.js
+++ src/scenes/Road.js
@@ -5,7 +5,7 @@ class Road extends Phaser.Scene{
     }
 
     preload(){
-        this.load.audio('sfx_grav', './assets/PowerUp1.wav');   
+        //this.load.audio('sfx_grav', './assets/PowerUp1.wav');   
     }
 
     create(){
